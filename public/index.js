203e1d4629b9cf8ba5f7715e824c59dca19d3e10
PurpleHurt <91793323+PurpleHurt@users.noreply.github.com>
2022-08-14 13:25:40 -0500

    added api key and symbols to request

--- public/index.js
+++ public/index.js
@@ -4,6 +4,16 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=42147f9c1c454321b889c6b8d8c4ee44`)
+
+    const result = await response.json()
+
+    const { GME, MSFT, DIS, BNTX } = result;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    
+    
 }
 
 main()
